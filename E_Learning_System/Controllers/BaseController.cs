8941a8b0fc8855ffd9d3f83e4af8fe9066a20306
Amr Elshaer <amrelsher07@gmail.com>
2022-07-24 10:59:26 +0200

    add error handling

--- E_Learning_System/Controllers/BaseController.cs
+++ E_Learning_System/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using Mediator.Net;
+using System;
 using System.Web.Mvc;
 
 namespace E_Learning_System.Controllers
@@ -6,6 +7,19 @@ namespace E_Learning_System.Controllers
     public class BaseController : Controller
     {
         protected IMediator Mediator => DependencyResolver.Current.GetService<IMediator>();
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Exception ex = filterContext.Exception;
+            filterContext.ExceptionHandled = true;
 
+            var model = new HandleErrorInfo(filterContext.Exception, "Home", "Error");
+
+            filterContext.Result = new ViewResult()
+            {
+                ViewName = "Error",
+                ViewData = new ViewDataDictionary(model)
+            };
+
+        }
     }
 }
