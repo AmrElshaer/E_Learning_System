ac91eb7560e280611df000f64773b9d7b2070009
Amr Elshaer <amrelsher07@gmail.com>
2022-07-25 20:14:34 +0200

    add-identity

---
+++ ELearning.Infrasturcture/Identity/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ELearning.Infrasturcture.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+           : base("name=ApplicationUsers")
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ E_Learning_System/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(E_Learning_System.Startup))]
+
+namespace E_Learning_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
